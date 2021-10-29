a0f1ae058286a99b03573915e4f7a20195a74fcf
JoseBorbon <joserborbon@gmail.com>
2021-10-29 15:10:40 -0400

    convert to class component and use pseudo form

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,5 +1,41 @@
-const DonationForm = () => {
-  return null;
-};
+import React, { Component } from 'react';
+
+class DonationForm extends Component {
+  render() {
+    return (
+      <section className="donation">
+        <h3>
+          You could be donation{' '}
+          <span className="secondary">{`#${this.props.donoId}!`}</span>
+        </h3>
+        <form>
+          <label>
+            Name
+            <input
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Your name..."
+            />
+          </label>
+          <label>
+            Caption
+            <input
+              id="caption"
+              name="caption"
+              type="text"
+              placeholder="Add a brief message..."
+            />
+          </label>
+          <label htmlFor="amount">
+            Amount
+            <input id="amount" name="amount" type="number" placeholder="0" />
+          </label>
+          <button>Donate!</button>
+        </form>
+      </section>
+    );
+  }
+}
 
 export default DonationForm;

80a0d40161404b81a5a671db49012c60d651b0eb
JoseBorbon <joserborbon@gmail.com>
2021-10-29 15:12:20 -0400

    convert to class component and import react

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -1,12 +1,16 @@
-const TopBar = () => {
-  return (
-    <header>
-      <h1>
-        GoFund<span>Me</span>
-      </h1>
-      <p>Help me go on a vacation to a beach somewhere!</p>
-    </header>
-  );
-};
+import React, { Component } from 'react';
+
+class TopBar extends Component {
+  render() {
+    return (
+      <header>
+        <h1>
+          GoFund<span>Me</span>
+        </h1>
+        <p>Help me go on a vacation to a beach somewhere!</p>
+      </header>
+    );
+  }
+}
 
 export default TopBar;

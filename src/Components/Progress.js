0a1ebf73d072d2035d7f0d8089148eb091345719
JoseBorbon <joserborbon@gmail.com>
2021-10-29 15:11:09 -0400

    convert to class component and use props

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,16 @@
-const Progress = () => {
-  return null;
-};
+import React, { Component } from 'react';
+
+class Progress extends Component {
+  render() {
+    return (
+      <section className="progress">
+        <h2>
+          Raised <span className="secondary">${this.props.total}</span> of
+          <span className="secondary">{` $${this.props.targetAmount}`}</span>
+        </h2>
+      </section>
+    );
+  }
+}
 
 export default Progress;

ae2dd101fd569988ad43477ec0f2721583e7f4d5
JoseBorbon <joserborbon@gmail.com>
2021-10-29 15:11:52 -0400

    iterate over donations and created dumb components

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,22 @@
-const RecentDonations = () => {
-  return null;
-};
+import React, { Component } from 'react';
+import Donation from './Donation';
+
+class RecentDonations extends Component {
+  render() {
+    const recentDonations = this.props.donations.map(
+      ({ name, amount, caption, id }) => {
+        return (
+          <Donation key={id} name={name} amount={amount} caption={caption} />
+        );
+      }
+    );
+    return (
+      <section>
+        <h2>Recent Donations</h2>
+        <ul>{recentDonations}</ul>
+      </section>
+    );
+  }
+}
 
 export default RecentDonations;

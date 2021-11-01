5d286cb0bf10017da5c0d29794f08ea085019fa5
salrey <tekdehn@gmail.com>
2021-11-01 00:30:01 -0400

    Add html for Progress and refactor, including props

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,15 @@
-const Progress = () => {
-  return null;
+import { Component } from "react";
+
+class Progress extends Component {
+  render () {
+    return (
+    <section className="progress">
+    <h2>
+      Raised <span className="secondary">${this.props.donations.reduce((accumulator, currDonation) => accumulator + currDonation.amount , 0)}</span> of <span className="secondary">${this.props.targetAmount}</span>
+    </h2>
+    </section>
+    );
+  }
 };
 
 export default Progress;

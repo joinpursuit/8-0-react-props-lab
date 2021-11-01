ac7a4cdc09315a021a8897178d107100e3edfbcf
salrey <tekdehn@gmail.com>
2021-11-01 00:35:25 -0400

    Add Donation component and add html for RecentDonations and refactor

---
+++ src/Components/Donation.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Donation extends Component {
+    render() {
+        return (
+        <li><span>{this.props.name} donated ${this.props.amount}</span>{this.props.caption}</li>
+        )
+    }
+}
+
+export default Donation
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,18 @@
-const RecentDonations = () => {
-  return null;
+import { Component } from "react";
+import Donation from "./Donation";
+
+class RecentDonations extends Component {
+  render() {
+    return (
+    <section>
+    <h2>Recent Donations</h2>
+    <ul>
+      {this.props.donations.map(({name, amount, caption}) => <Donation name={name} amount={amount} caption={caption} />)}
+      {/* {this.props.donations.map((donation) => <li><span>{donation.name} donated ${donation.amount}</span>{donation.caption}</li>)} */}
+    </ul>
+    </section>
+    );
+  }
 };
 
 export default RecentDonations;

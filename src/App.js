b9d8a3dab4ba23d88ed73d02de21076eb5235300
JoseBorbon <joserborbon@gmail.com>
2021-11-03 13:39:26 -0400

    made App dynamic with state

--- src/App.js
+++ src/App.js
@@ -5,56 +5,84 @@ import DonationForm from './Components/DonationForm';
 import RecentDonations from './Components/RecentDonations';
 import './App.css';
 
-const targetAmount = 1000;
-const donations = [
-  {
-    amount: 250,
-    caption: 'You really need this. Really.',
-    id: 1,
-    name: 'Jo',
-  },
-  {
-    amount: 30,
-    caption: 'Here, take a break from work!',
-    id: 2,
-    name: 'Rami',
-  },
-  {
-    amount: 20,
-    caption: 'LOL! You are too funny. Happy to do this for you. :)',
-    id: 3,
-    name: 'Michelle',
-  },
-  {
-    amount: 5,
-    caption: 'Have fun!',
-    id: 4,
-    name: 'Malinda',
-  },
-  {
-    amount: 30,
-    caption: 'Come visit me in Miami!',
-    id: 5,
-    name: 'Sam',
-  },
-];
-
 export default class App extends React.Component {
-  static defaultProps = {
-    total: donations.reduce((total, { amount }) => total + amount, 0),
-    donoId: donations[donations.length - 1].id + 1,
-  };
+  static defaultProps = { targetAmount: 1000 };
+  constructor(props) {
+    super(props);
+    this.state = {
+      donations: [
+        {
+          amount: 250,
+          caption: 'You really need this. Really.',
+          id: 1,
+          name: 'Jo',
+        },
+        {
+          amount: 30,
+          caption: 'Here, take a break from work!',
+          id: 2,
+          name: 'Rami',
+        },
+        {
+          amount: 20,
+          caption: 'LOL! You are too funny. Happy to do this for you. :)',
+          id: 3,
+          name: 'Michelle',
+        },
+        {
+          amount: 5,
+          caption: 'Have fun!',
+          id: 4,
+          name: 'Malinda',
+        },
+        {
+          amount: 30,
+          caption: 'Come visit me in Miami!',
+          id: 5,
+          name: 'Sam',
+        },
+      ],
+      nextId: 6,
+    };
+    this.addDonation = this.addDonation.bind(this);
+  }
+
+  addDonation(donation) {
+    const { nextId } = this.state;
+    const newDonation = {
+      ...donation,
+      nextId,
+      amount: Number(donation.amount),
+    };
+    this.setState((st) => {
+      return {
+        donations: [...st.donations.slice(), newDonation],
+        nextId: st.nextId + 1,
+      };
+    });
+  }
+
   render() {
+    const total = this.state.donations.reduce(
+      (total, { amount }) => total + amount,
+      0
+    );
     return (
       <>
         <TopBar />
         <main className="container">
           <section className="sidebar">
-            <RecentDonations donations={donations} />
+            <RecentDonations
+              donations={this.state.donations}
+              key={this.state.nextId}
+            />
           </section>
           <section className="">
-            <Progress total={this.props.total} targetAmount={targetAmount} />
-            <DonationForm donoId={this.props.donoId} />
+            <Progress total={total} targetAmount={this.props.targetAmount} />
+            <DonationForm
+              donoId={this.state.nextId}
+              addDonation={this.addDonation}
+            />
           </section>
         </main>
       </>

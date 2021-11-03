7d319aa6db4051a0f528bf3fac2a96660e368f96
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-11-03 14:20:15 -0400

    Done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import "./App.css";
 import React from "react";
 import TopBar from "./Components/TopBar";
-import "./App.css";
+import Progress from "./Components/Progress";
+import DonationForm from "./Components/DonationForm";
+import RecentDonations from "./Components/RecentDonations";
 
 const targetAmount = 1000;
 const donations = [
@@ -40,12 +43,14 @@ export default class App extends React.Component {
   render() {
     return (
       <>
-        <TopBar />
+        <TopBar/>
         <main className="container">
-          <section className="sidebar">{/* Recent Donations */}</section>
+          <section className="sidebar">
+            <RecentDonations donations={donations}/>
+          </section>
           <section className="">
-            {/* Progress */}
-            {/* Donation Form */}
+            <Progress max={targetAmount}/>
+            <DonationForm donations={donations.length + 1}/>
           </section>
         </main>
       </>

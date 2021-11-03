89f108f95b024614d476459dce8355613b611bd6
JoseBorbon <joserborbon@gmail.com>
2021-11-03 13:40:14 -0400

    add state for html form

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 
 class DonationForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', caption: '', amount: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState((st) => {
+      return { [evt.target.name]: evt.target.value };
+    });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+    const { name, caption, amount } = this.state;
+    this.props.addDonation({ name, caption, amount });
+    this.setState({ name: '', caption: '', amount: 0 });
+  }
+
   render() {
     return (
       <section className="donation">
@@ -8,7 +28,7 @@ class DonationForm extends Component {
           You could be donation{' '}
           <span className="secondary">{`#${this.props.donoId}!`}</span>
         </h3>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>
             Name
             <input
@@ -16,6 +36,8 @@ class DonationForm extends Component {
               name="name"
               type="text"
               placeholder="Your name..."
+              value={this.state.name}
+              onChange={this.handleChange}
             />
           </label>
           <label>
@@ -25,13 +47,26 @@ class DonationForm extends Component {
               name="caption"
               type="text"
               placeholder="Add a brief message..."
+              value={this.state.caption}
+              onChange={this.handleChange}
             />
           </label>
-          <label htmlFor="amount">
+          <label>
             Amount
-            <input id="amount" name="amount" type="number" placeholder="0" />
+            <input
+              id="amount"
+              name="amount"
+              type="number"
+              placeholder="0"
+              value={this.state.amount}
+              onChange={this.handleChange}
+            />
           </label>
-          <button>Donate!</button>
+          <input
+            type="submit"
+            value="Donate!"
+            className="donation-form-button "
+          />
         </form>
       </section>
     );

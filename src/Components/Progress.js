567645f3d50d0a4d78cea17a821bb2b16992a5bb
Triane Peart <TrianePeart@gmail.com>
2021-11-03 14:19:45 -0400

    Completed

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,10 @@
-const Progress = () => {
-  return null;
+const Progress = (promps) => {
+  return( <section class="progress">
+  <h2>
+    Raised <span class="secondary">$335</span> of
+    <span class="secondary"> ${promps.max}</span>
+  </h2>
+</section>)
 };
 
 export default Progress;
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,23 @@
-const RecentDonations = () => {
-  return null;
+const RecentDonations = (promp) => {
+  let recent = promp.donations.map((item) => {
+    return (
+      <li>
+        <span>
+          {item.name} donated ${item.amount}
+        </span>
+        {item.caption}
+      </li>
+    )
+  });
+  return (
+    
+    <section>
+    <h2>Recent Donations</h2>
+    <ul>
+      {recent}
+    </ul>
+  </section>
+    )
 };
 
 export default RecentDonations;

1230ef147eab11b8590d0a5df80f68cc4e35317f
Fatema Kabir <fatema.kabir@pursuit.org>
2021-11-03 19:49:29 -0400

    work in progress

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,13 @@
-const Progress = () => {
-  return null;
+const Progress = (props) => {
+  
+  return (
+    <section class="progress">
+  <h2>
+    Raised <span class="secondary">$335</span> of
+    <span class="secondary"> ${props.max}</span>
+  </h2>
+</section>
+  )
 };
 
 export default Progress;

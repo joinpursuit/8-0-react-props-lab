c1b62adbd19a4e4e9a534333288c02e27e5e4c3e
Sara Perkaj <saraperkaj@pursuit.org>
2021-11-03 16:57:18 -0400

    Done (:

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,19 @@
-const Progress = () => {
-  return null;
+const Progress = (props) => {
+  return (
+    <section className="progress">
+      <h2>
+        Raised{" "}
+        <span className="secondary">
+          $
+          {props.donations.reduce((acc, val) =>
+            acc.amount ? acc.amount + val.amount : acc + val.amount
+          )}
+        </span>{" "}
+        of
+        <span className="secondary"> ${props.max}</span>
+      </h2>
+    </section>
+  );
 };
 
 export default Progress;

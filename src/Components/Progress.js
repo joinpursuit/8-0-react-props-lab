b0c15e9fbbafbf85ab87a059c16bba474bc72cba
Lakisha J <lakishajohnson@pursuit.org>
2021-11-03 12:37:02 -0400

    Progress w/ props

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,14 @@
-const Progress = () => {
-  return null;
+const Progress = (props) => {
+  return (
+  <section class="progress">
+  <h2>
+    Raised <span class="secondary">$335</span> of
+    <span class="secondary"> ${props.max}</span>
+    {/* props is still an object, it just has one property on it right now; max */}
+  </h2>
+</section>
+  )
 };
+
 
 export default Progress;

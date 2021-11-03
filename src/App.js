29aba8d0358c6b9cdc092a6ed74d34b9049500c9
DylanLovell <arcanyne7@gmail.com>
2021-11-03 14:40:17 -0400

    small error fixes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
-import React from "react";
-import TopBar from "./Components/TopBar";
-import "./App.css";
+import React from 'react';
+import TopBar from './Components/TopBar';
+import Progress from './Components/Progress';
+import DonationForm from './Components/DonationForm';
+import './App.css';
+import RecentDonations from './Components/RecentDonations';
 
 const targetAmount = 1000;
 const donations = [
   {
     amount: 250,
-    caption: "You really need this. Really.",
+    caption: 'You really need this. Really.',
     id: 1,
-    name: "Jo",
+    name: 'Jo',
   },
   {
     amount: 30,
-    caption: "Here, take a break from work!",
+    caption: 'Here, take a break from work!',
     id: 2,
-    name: "Rami",
+    name: 'Rami',
   },
   {
     amount: 20,
-    caption: "LOL! You are too funny. Happy to do this for you. :)",
+    caption: 'LOL! You are too funny. Happy to do this for you. :)',
     id: 3,
-    name: "Michelle",
+    name: 'Michelle',
   },
   {
     amount: 5,
-    caption: "Have fun!",
+    caption: 'Have fun!',
     id: 4,
-    name: "Malinda",
+    name: 'Malinda',
   },
   {
     amount: 30,
-    caption: "Come visit me in Miami!",
+    caption: 'Come visit me in Miami!',
     id: 5,
-    name: "Sam",
+    name: 'Sam',
   },
 ];
 
@@ -42,10 +45,12 @@ export default class App extends React.Component {
       <>
         <TopBar />
         <main className="container">
-          <section className="sidebar">{/* Recent Donations */}</section>
+          <section className="sidebar">
+            <RecentDonations donations={donations} />
+          </section>
           <section className="">
-            {/* Progress */}
-            {/* Donation Form */}
+            <Progress max={targetAmount} donations={donations} />
+            <DonationForm donations={donations} />
           </section>
         </main>
       </>
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,19 @@
-const RecentDonations = () => {
-  return null;
+const RecentDonations = (props) => {
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        {props.donations.map((item) => (
+          <li key={item.id}>
+            <span>
+              {item.name} donated ${item.amount}
+            </span>
+            {item.caption}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default RecentDonations;

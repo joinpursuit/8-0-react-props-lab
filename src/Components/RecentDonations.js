60dbded7801bce9c86f0d0ac460d402d42d6296a
Lakisha J <lakishajohnson@pursuit.org>
2021-11-03 14:43:43 -0400

    RecentDonations w/ props

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,19 @@
-const RecentDonations = () => {
-  return null;
+const RecentDonations = (props) => {
+  let recent = props.donations.map((donation) => {
+    return (
+      <li>
+        <span>
+          {donation.name} donated ${donation.amount}
+        </span>
+        {donation.caption}
+      </li>
+    );
+  });
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>{recent}</ul>
+    </section>
+  );
 };
-
 export default RecentDonations;

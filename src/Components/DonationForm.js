280b6573b12bf038bb9c4da4e0d9f3a757cc8f19
Lakisha J <lakishajohnson@pursuit.org>
2021-11-03 15:23:01 -0400

    DonationForm w/ props

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -5,16 +5,25 @@ const DonationForm = (props) => {
       </span></h3>
       <form>
         <label>
-          Name
-          <input id="name" name="name" type="text" placeholder="Your name..." />
+          Name<input 
+          id="name" 
+          name="name" 
+          type="text" 
+          placeholder="Your name..." />
         </label>
         <label>
-          Caption
-          <input id="caption" name="caption" type="text" placeholder="Add a brief message..." />
+          Caption<input 
+          id="caption" 
+          name="caption" 
+          type="text" 
+          placeholder="Add a brief message..." />
         </label>
         <label>
-          Amount
-          <input id="amount" name="amount" type="number" placeholder="0" />
+          Amount<input 
+          id="amount" 
+          name="amount" 
+          type="number" 
+          placeholder="0" />
         </label>
         <button>Donate!</button>
       </form>

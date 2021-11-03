a5091a62ae64875713aa6368b75013bc263bfeb0
Lakisha J <lakishajohnson@pursuit.org>
2021-11-03 14:44:57 -0400

    Modified for interpolation

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -2,7 +2,7 @@ const Progress = (props) => {
   return (
   <section class="progress">
   <h2>
-    Raised <span class="secondary">$335</span> of
+    Raised <span class="secondary">${props.min}</span> of
     <span class="secondary"> ${props.max}</span>
     {/* props is still an object, it just has one property on it right now; max */}
   </h2>

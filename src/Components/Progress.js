8b887eb8cd12538203b01aee03ab59eaeb95874e
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2021-11-03 23:06:36 -0400

    React-Props Lab

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,19 @@
-const Progress = () => {
-  return null;
+const Progress = (props) => {
+  let contribution = props.currentValue.map((element) => {
+    return element.amount;
+  });
+
+  contribution = contribution.reduce((a, b) => {
+    return a + b;
+  });
+  return (
+    <section class="progress">
+      <h2>
+        Raised <span class="secondary"> ${contribution}</span> of
+        <span class="secondary"> ${props.max}</span>
+      </h2>
+    </section>
+  );
 };
 
 export default Progress;
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -3,3 +3,15 @@ const RecentDonations = () => {
 };
 
 export default RecentDonations;
+<section>
+  <h2>Recent Donations</h2>
+  <ul>
+    <li>
+      <span>Jo donated $25</span>You really need this. Really.
+    </li>
+    <li>
+      <span>Rami donated $10</span>Here, take a break from work!
+    </li>
+    {/* <!-- etc... --> */}
+  </ul>
+</section>;

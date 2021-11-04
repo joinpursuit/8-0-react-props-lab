2321408ce79759541bfa6069abc7a45e626b150b
Name Ping Louie <namepinglouie@gmail.com>
2021-11-03 20:46:27 -0400

    struggled to finish the lab

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,28 @@
+/*
 const Progress = () => {
   return null;
 };
+*/
+
+import { Component } from "react";
+
+class Progress extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    const {donations} = this.props;
+    let total = donations.map(donated => donated.amount).reduce((a, b) => a + b, 0)
+    
+    return(
+      <section class="progress">
+        <h2>Raised <span class="secondary">${total}</span> of
+        <span class="secondary"> $1000</span>
+        </h2>
+      </section>
+    )
+  }
+}
+
 
 export default Progress;
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,28 @@
+/*
 const RecentDonations = () => {
   return null;
 };
+*/
+
+import { Component } from "react";
+
+class RecentDonations extends Component {
+  constructor() {
+    super(); 
+  }
+
+  render() {
+    const donationList = this.props.donations.map((donated) => {
+      return <li><span>{donated.name} donated ${donated.amount}</span>{donated.caption}</li>;
+    })
+
+    return(
+      <section>
+        <h2>Recent Donations</h2>
+        <ul>{donationList}</ul>
+      </section>
+    )
+  }
+}
 
 export default RecentDonations;

9157575ee92160ac0dd497b72647ac06bd5311e6
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-04 19:28:52 -0400

    passed all tests. Added total amount props to Progress.js to add all donations together

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import donations from "../data/donations";
 
 class Progress extends Component{
   constructor(){
@@ -6,11 +7,17 @@ class Progress extends Component{
   }
 
   render(){
+    let total = 0;
+    donations.map((donation)=>{
+      return ( total += donation.amount )
+    })
+
+
     return(
       <section class="progress">
         <h2>
-          Raised <span class="secondary">$0</span> of
-          <span class="secondary">$1000</span>
+          Raised <span class="secondary">${ total }</span> of
+          <span class="secondary"> $1000</span>
         </h2>
       </section>
     )

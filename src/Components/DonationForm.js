7d9c5647af29e46af71f6ccce7453ee0e082df0c
Bryant Mercedes <bryantmercedescanela@gmail.com>
2021-11-03 22:18:53 -0400

    finished

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,5 +1,25 @@
-const DonationForm = () => {
-  return null;
+const DonationForm = (props) => {
+  return (
+    <section class="donation">
+      <h3>
+        You could be donation <span class="secondary">#{props.ordered}!</span>
+      </h3>
+      <form>
+        <label>Name</label>
+        <input id="name" name="name" type="text" placeholder="Your name..." />
+        <label>Caption</label>
+        <input
+          id="caption"
+          name="caption"
+          type="text"
+          placeholder="Add a brief message..."
+        />
+        <label for="amount">Amount</label>
+        <input id="amount" name="amount" type="number" placeholder="0" />
+        <button>Donate!</button>
+      </form>
+    </section>
+  );
 };
 
 export default DonationForm;
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,23 @@
-const RecentDonations = () => {
-  return null;
+const RecentDonations = ({ donations }) => {
+  const result = donations.map((donation) => {
+    const { name, amount, caption } = donation;
+
+    return (
+      <li>
+        <span>
+          {name} ${amount}
+        </span>
+        {caption}
+      </li>
+    );
+  });
+
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>{result}</ul>
+    </section>
+  );
 };
 
 export default RecentDonations;

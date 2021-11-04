b7d9e7060bc5ca4868996bb93a49b5ad3a71bc2a
stephanie lucano <stephaniemarielucano@gmail.com>
2021-11-04 00:35:49 -0400

    done

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,21 @@
-const RecentDonations = () => {
-  return null;
+const RecentDonations = (props) => {
+  console.log(props, props.donations)
+  let recent = props.donations.map(({name, amount, caption}) => {
+    return (
+      <li>
+        <span>
+          {name} donated {"$"+amount}
+        </span>
+          {caption}
+      </li>
+    )
+  })
+  return (
+    <section>
+  <h2>Recent Donations</h2>
+  <ul>{recent}</ul>
+</section>
+  );
 };
 
 export default RecentDonations;

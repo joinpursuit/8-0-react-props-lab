b14f08af7e4afe8f5a48b6132c1f488bf2b010b4
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-04 23:12:07 -0400

    error fixed maybe

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import TopBar from "./components/TopBar";
-import Progress from "./components/Progress";
-import DonationForm from "./components/DonationForm";
-import RecentDonations from "./components/RecentDonations";
+import TopBar from "./Components/TopBar";
+import Progress from "./Components/Progress";
+import DonationForm from "./Components/DonationForm";
+import RecentDonations from "./Components/RecentDonations";
 import "./App.css";
 
 const targetAmount = 1000;
--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -1,20 +1,12 @@
-import React from "react";
-
-class TopBar extends React.Component{
-  // constructor(){
-  //   super();
-  // }
-
-  render(){
-    return (
-      <header>
-        <h1>
-          GoFund<span>Me</span>
-        </h1>
-        <p>Help me go on a vacation to a beach somewhere!</p>
-      </header>
-    )
-  }
-}
+const TopBar = () => {
+  return (
+    <header>
+      <h1>
+        GoFund<span>Me</span>
+      </h1>
+      <p>Help me go on a vacation to a beach somewhere!</p>
+    </header>
+  );
+};
 
 export default TopBar;

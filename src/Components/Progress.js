730dc88e2fb2a1f649ceae9c854b94fdf99c1207
Angie G <angieg@Angies-MBP.fios-router.home>
2021-11-05 00:50:59 -0400

    Progress File

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,12 +1,13 @@
 const Progress = (props) => {
+  console.log(props.raised);
   return (
     <section class="progress">
-  <h2>
-    Raised <span class="secondary">$0</span> of
-    <span class="secondary">{props.max}</span>
-  </h2>
-</section>
-  )
+      <h2>
+        Raised <span class="secondary">{"$" + props.raised}</span> of
+        <span class="secondary">${props.max}</span>
+      </h2>
+    </section>
+  );
 };
 
 export default Progress;

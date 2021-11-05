72a8a2b24fa83b1c1f8d5fd189512c9e1faad025
Angie G <angieg@Angies-MBP.fios-router.home>
2021-11-05 00:52:29 -0400

    RecentDonations.js

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,22 @@
 const RecentDonations = () => {
-  return null;
+  console.log(props, props.donations);
+  let recent = props.donations.map(({ name, amount, caption }) => {
+    return (
+      <li>
+        <span>
+          {name} donated {"$" + amount}
+        </span>
+          {caption}
+      </li>
+    );
+  });
+
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>{recent}</ul>
+    </section>
+  );
 };
 
 export default RecentDonations;

243f474a9ac4aeca80c19289f4c447c7f203f88a
Xiaoming Yang <yangxiaoming295@gmail.com>
2021-11-04 23:18:20 -0400

    create recentdonations.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TopBar from "./Components/TopBar";
+import RecentDonations from "./Components/RecentDonations";
 import "./App.css";
 
 const targetAmount = 1000;
@@ -45,7 +46,7 @@ export default class App extends React.Component {
           <section className="sidebar">
             <RecentDonations donations={donations} />
           </section>
-          <section className="">
+          <section className="progress">
             <Progress max={targetAmount} />
             <DonationForm donations={donations.length +1} />
           </section>
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,22 @@
-const RecentDonations = () => {
-  return null;
+import React from "react";
+
+const RecentDonations = (props) => {
+  const UserDonations = props.donations.map((donor) => {
+    return (
+      <li>
+        <span>
+          {donor.name} donated ${donor.amount}
+        </span>
+        {donor.caption}
+      </li>
+    )
+  })
+  return (
+    <section>
+      <h2> Recent Donations </h2>
+      <ul> {UserDonations} </ul>
+    </section>
+  );
 };
 
 export default RecentDonations;

da530df89d55de1a8b69a439a788d1220265d813
Nima sherpa <Toplouie78@gmail.com>
2021-11-07 13:17:42 -0500

    Test passed

--- src/App.js
+++ src/App.js
@@ -1,54 +1,63 @@
 import React from "react";
 import TopBar from "./Components/TopBar";
 import "./App.css";
+import RecentDonations from "./Components/RecentDonations";
+import Progress from "./Components/Progress";
+import DonationForm from "./Components/DonationForm";
 
 const targetAmount = 1000;
 const donations = [
-  {
-    amount: 250,
-    caption: "You really need this. Really.",
-    id: 1,
-    name: "Jo",
-  },
-  {
-    amount: 30,
-    caption: "Here, take a break from work!",
-    id: 2,
-    name: "Rami",
-  },
-  {
-    amount: 20,
-    caption: "LOL! You are too funny. Happy to do this for you. :)",
-    id: 3,
-    name: "Michelle",
-  },
-  {
-    amount: 5,
-    caption: "Have fun!",
-    id: 4,
-    name: "Malinda",
-  },
-  {
-    amount: 30,
-    caption: "Come visit me in Miami!",
-    id: 5,
-    name: "Sam",
-  },
+	{
+		amount: 250,
+		caption: "You really need this. Really.",
+		id: 1,
+		name: "Jo",
+	},
+	{
+		amount: 30,
+		caption: "Here, take a break from work!",
+		id: 2,
+		name: "Rami",
+	},
+	{
+		amount: 20,
+		caption: "LOL! You are too funny. Happy to do this for you. :)",
+		id: 3,
+		name: "Michelle",
+	},
+	{
+		amount: 5,
+		caption: "Have fun!",
+		id: 4,
+		name: "Malinda",
+	},
+	{
+		amount: 30,
+		caption: "Come visit me in Miami!",
+		id: 5,
+		name: "Sam",
+	},
 ];
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <>
-        <TopBar />
-        <main className="container">
-          <section className="sidebar">{/* Recent Donations */}</section>
-          <section className="">
-            {/* Progress */}
-            {/* Donation Form */}
-          </section>
-        </main>
-      </>
-    );
-  }
+	render() {
+		let recentList = donations.map((each) => {
+			return <RecentDonations each={each} />;
+		});
+		return (
+			<>
+				<TopBar />
+				<main className="container">
+					<section className="sidebar">
+						<h2>Recent Donations</h2>
+						{recentList}
+					</section>
+					<section>
+						<Progress targetAmount={targetAmount} donations={donations} />
+						<DonationForm />
+					</section>
+				</main>
+			</>
+		);
+	}
 }
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,19 @@
-const RecentDonations = () => {
-  return null;
-};
+import React, { Component } from "react";
 
-export default RecentDonations;
+export default class RecentDonations extends Component {
+	render() {
+		const { name, amount, caption } = this.props.each;
+		return (
+			<section>
+				<ul>
+					<li>
+						<span>
+							{name} donated ${amount}
+						</span>
+						{caption}
+					</li>
+				</ul>
+			</section>
+		);
+	}
+}

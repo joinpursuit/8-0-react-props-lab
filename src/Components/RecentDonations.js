d13ed03975979cb8226f8a97a4d227438ba7a841
Larisa <larisashapkinab@gmail.com>
2021-11-08 21:19:04 -0500

    8-0-react-props-lab-done

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,26 @@
-const RecentDonations = () => {
-  return null;
-};
+import { Component } from "react";
+
+class RecentDonations extends Component {
+  constructor(){
+    super()
+  }
+
+  render(){
+    const {donations} = this.props;
+    const allDonations = donations.map((donation)=>{
+      return (
+        <li><span>{donation.name} donated ${donation.amount}</span>{donation.caption}</li>);
+    })
+
+    return(
+      <section>
+        <h2>Recent Donations</h2>
+        <ul>
+        {allDonations}
+        </ul>
+    </section>
+    )
+  }
+} 
 
 export default RecentDonations;
--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -1,12 +1,20 @@
-const TopBar = () => {
-  return (
-    <header>
-      <h1>
-        GoFund<span>Me</span>
-      </h1>
-      <p>Help me go on a vacation to a beach somewhere!</p>
-    </header>
-  );
-};
+import { Component } from "react";
+
+class TopBar extends Component {
+  constructor (){
+    super()
+  }
+  
+  render(){
+    return (
+      <header>
+        <h1>
+          GoFund<span>Me</span>
+        </h1>
+        <p>Help me go on a vacation to a beach somewhere!</p>
+      </header>
+    );
+  }
+}
 
 export default TopBar;

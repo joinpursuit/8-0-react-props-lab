7520ac12b017c7f4013d13dbed48f6541b9a9a2b
hinkabi <hinkabi@gmail.com>
2021-11-09 22:20:25 -0500

    2 of 3 test passing

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,18 @@
-const Progress = () => {
-  return null;
-};
+import { Component } from "react";
+
+class Progress extends Component {
+  render() {
+    
+    return (
+      <section class="progress">
+        <h2>
+          Raised <span class="secondary">${this.props.totalDonations}</span>
+          of {" "}
+          <span class="secondary">${this.props.targetAmount}</span>
+        </h2>
+      </section>
+    );
+  }
+}
 
 export default Progress;
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,21 @@
-const RecentDonations = () => {
-  return null;
+import { Component } from "react";
+
+class RecentDonations extends Component {
+
+  render(){
+    //const { donations } = this.props.donations;
+    const donationsArr = this.props.donations.map((donation, index)=>{
+      return <li key={index}><span>{donation.name} donated ${donation.amount}</span>{donation.caption}</li>
+    })
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        {donationsArr}
+      </ul>
+    </section>
+  );
+  }
 };
 
 export default RecentDonations;

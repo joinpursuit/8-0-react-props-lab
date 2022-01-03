41d18d4073e05e30ef7980a69d9e5b70b4331d4e
shareekaepps <shareekaepps@icloud.com>
2022-01-03 00:37:37 -0500

    lab donce, cy issues

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,27 @@
-const Progress = () => {
-  return null;
+import { Component } from "react";
+
+class Progress extends Component {
+  constructor() {
+  super();
+  }
+
+render() {
+  const { donations, targetAmount } = this.props;
+  let total = 0;
+
+  donations.forEach((donation) => {
+  return total += donation.amount;
+  })
+
+  return (
+  <section class="progress" >
+    <h2>
+    Raised<span class="secondary"> $ { total } </span> of
+    <span class="secondary"> $ { targetAmount }</span>
+    </h2>
+  </section>
+    )
+  }  
 };
 
 export default Progress;

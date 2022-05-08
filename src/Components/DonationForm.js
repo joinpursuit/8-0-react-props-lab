f82222ed9677a0e6a094223c6b60bf2edafc3f27
Mendoza Holger <mendozadavidh111@gmail.com>
2022-05-08 17:46:25 -0400

    lab completed passing all tests

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,5 +1,46 @@
-const DonationForm = () => {
-  return null;
+const DonationForm = ({ donations}) => {
+  return (
+    <div>
+      <section className="donation">
+        <h3>You could be donation #<span className="secondary">
+          {donations.length + 1}!
+            </span></h3>
+        <form>
+          <label>
+            Name
+            <br />
+            <input
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Your name..."
+            />
+          </label>
+          <label>
+            Caption
+            <br />
+            <input
+              id="caption"
+              name="caption"
+              type="text"
+              placeholder="Add a brief message..."
+            />
+          </label>
+          <label for="amount">
+            Amount
+            <br />
+            <input id="amount" name="amount" type="number" placeholder="0" />
+          </label>
+          <br />
+          <button>Donate!</button>
+        </form>
+      </section>
+    </div>
+  );
 };
 
 export default DonationForm;
+
+
+
+

bccd88b1e637fa96596fc6387bee7b03ad1b1187
Samantha Figueroa <samanthafigueroa@pursuit.org>
2022-05-08 17:43:04 -0400

    lab done

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,17 @@
-const Progress = () => {
-  return null;
+const Progress = (props) => {
+  let sum = 0;
+  props.donations.map(item => {
+    sum += item.amount;
+  })
+
+  return (
+    <section class="progress">
+      <h2>
+        Raised <span class="secondary">${sum}</span> of
+        <span class="secondary"> ${props.max}</span>
+      </h2>
+    </section>
+  );
 };
 
 export default Progress;

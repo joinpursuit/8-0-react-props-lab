b516751a6ce439f9602fbe2c2d4952d47da335d4
Spencer Simon <spencersimon@pursuit.org>
2022-05-09 18:56:47 -0400

    React Props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
-import TopBar from "./Components/TopBar";
+import TopBar from "./Components/TopBar.js";
+import Progress from "./Components/Progress.js";
+import DonationForm from "./Components/DonationForm.js";
+import RecentDonations from "./Components/RecentDonations.js";
 import "./App.css";
 
 const targetAmount = 1000;
@@ -36,16 +39,33 @@ const donations = [
   },
 ];
 
+let numberCount = 0;
+let totalReceived = 0;
+donations.forEach((donor) => {
+  totalReceived += donor.amount;
+  numberCount += 1;
+});
+
 export default class App extends React.Component {
   render() {
     return (
       <>
         <TopBar />
         <main className="container">
-          <section className="sidebar">{/* Recent Donations */}</section>
+          <section className="sidebar">
+            {donations.map((donor) => {
+              return (
+                <RecentDonations
+                  name={donor.name}
+                  amount={donor.amount}
+                  caption={donor.caption}
+                />
+              );
+            })}
+          </section>
           <section className="">
-            {/* Progress */}
-            {/* Donation Form */}
+            <Progress target={targetAmount} totalRaised={totalReceived} />
+            <DonationForm number={numberCount} />
           </section>
         </main>
       </>
--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,12 @@
-const Progress = () => {
-  return null;
-};
+import React from "react";
 
-export default Progress;
+export default function Progress(props) {
+  return (
+    <section class="progress">
+      <h2>
+        Raised <span class="secondary">${props.totalRaised}</span> of
+        <span class="secondary"> ${props.target}</span>
+      </h2>
+    </section>
+  );
+}

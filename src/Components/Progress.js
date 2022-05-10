7d9bee63d282a6217918e9eb07b50d96070f4eb2
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-05-10 09:24:33 -0300

    No tests pass, donation site functional.

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,15 @@
-const Progress = () => {
-  return null;
+import React from 'react';
+
+const Progress = (props) => {
+  const { targetAmount, donations } = props;
+  return (
+    <section class='progress'>
+      <h2>
+        Raised <span class='secondary'>$0</span> of
+        <span class='secondary'>$1000</span>
+      </h2>
+    </section>
+  );
 };
 
 export default Progress;

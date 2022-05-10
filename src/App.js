5e339d173b80fe651b33d946988ff43276f5f16b
Jonathan Scheiber <jon@scheiber.me>
2022-05-09 21:10:06 -0400

    import components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import TopBar from "./Components/TopBar";
 import "./App.css";
+import DonationForm from "./Components/DonationForm";
+import Progress from "./Components/Progress";
+import RecentDonations from "./Components/RecentDonations";
 
 const targetAmount = 1000;
 const donations = [

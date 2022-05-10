2e7a570c2d28a0e9644f7f8410c2374e1215f712
Edwin Codrington <edwincodrington@gmail.com>
2022-05-10 01:09:02 -0400

    completed lab, in spite of jsx

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,16 @@
-const Progress = () => {
-  return null;
+const Progress = (props) => {
+	const { donations } = props;
+	return (
+		<section class="progress">
+			<h2>
+				Raised{" "}
+				<span class="secondary">
+					${donations.reduce((prev, donation) => prev + donation.amount, 0)}
+				</span>{" "}
+				of <span class="secondary">$1000</span>
+			</h2>
+		</section>
+	);
 };
 
 export default Progress;

c79bd4e2725659ed86089005954264aeb89afcbc
JC-MT <jancarlosmatias@gmail.com>
2022-05-10 08:50:03 -0400

    all tests passing

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,23 @@
-const RecentDonations = () => {
-  return null;
+const RecentDonations = (props) => {
+  const { donations } = props;
+
+  const recentDonationBuilder = donations.map((donation) => {
+    return (
+      <li>
+        <span>
+          {donation.name} donated ${donation.amount}
+        </span>
+        {donation.caption}
+      </li>
+    );
+  });
+
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>{recentDonationBuilder}</ul>
+    </section>
+  );
 };
 
 export default RecentDonations;

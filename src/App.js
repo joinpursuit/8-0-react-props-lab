39d9360ebe441540feab9d7277fb1be1fb7dc844
Isaac Gonzalez <u1337x@gmail.com>
2022-06-07 13:17:20 -0400

    lab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import TopBar from "./Components/TopBar";
 import "./App.css";
+import DonationForm from "./Components/DonationForm";
+import Progress from "./Components/Progress";
+import RecentDonations from "./Components/RecentDonations";
 
 const targetAmount = 1000;
 const donations = [
@@ -36,16 +39,53 @@ const donations = [
   },
 ];
 
+const sumOfDonations = () => {
+  let sum = 0;
+  for (let i = 0; i < donations.length; i++) {
+    sum += donations[i].amount;
+  }
+  return sum;
+};
 export default class App extends React.Component {
   render() {
+    const recentDonationsList = donations.map((donated) => {
+      return (
+        <RecentDonations
+          name={donated.name}
+          amount={donated.amount}
+          caption={donated.caption}
+        />
+      );
+    });
     return (
       <>
         <TopBar />
         <main className="container">
-          <section className="sidebar">{/* Recent Donations */}</section>
           <section className="">
-            {/* Progress */}
-            {/* Donation Form */}
+            <h2>Recent Donation</h2>
+            {recentDonationsList}
+          </section>
+          <section>
+            <Progress currentAmount={sumOfDonations()} goal={targetAmount} />
+            <form>
+              <DonationForm
+                currentId={donations[donations.length - 1].id + 1}
+              />
+              <section>
+                <label>
+                  <p>Name</p>
+                  <input type="text" placeholder="Add a breif message..." />
+                </label>
+              </section>
+              <section>
+                <label>
+                  <p>Amount</p>
+                  <input type="text" placeholder="0" />
+                </label>
+              </section>
+              <br></br>
+              <input type="submit" value="Donate" />
+            </form>
           </section>
         </main>
       </>
--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,11 @@
-const Progress = () => {
-  return null;
+const Progress = ({ currentAmount, goal }) => {
+  return (
+    <div>
+      <h1>
+        Raised ${currentAmount} of ${goal}
+      </h1>
+    </div>
+  );
 };
 
 export default Progress;

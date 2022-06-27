e3e5b0fb58ba3ccd256c0548fa0d8cac76d2de7b
Jahvon <jahvondevine-jones41@pursuit.org>
2022-06-27 18:35:02 -0400

    submitting props lab

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,16 @@
 const Progress = () => {
-  return null;
+  // style = {width: "25%", 
+  // ariaValuenow: "25",
+  // ariaValuemin: "0", 
+  // ariaValuemax: "100"}
+  return ( 
+    <div>
+      <h2 className='mb-4'>Raised $441 of <span className='text-muted'>$1000</span></h2>
+        <div className="progress">
+          <div className="progress-bar bg-success" role="progressbar" >25%</div>
+        </div>
+    </div>
+  );
 };
 
 export default Progress;

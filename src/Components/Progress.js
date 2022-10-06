9fefa8030e1b9e67fc69db2fa15cbb2fb5c21b31
Salinamalek <salinamalek@pursuit.org>
2022-10-06 17:53:48 -0400

    all tests passed

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,15 @@
-const Progress = () => {
-  return null;
+const Progress = ({ donations, targetAmount }) => {
+  const donos = donations.reduce((accumulator, {amount}) => {
+    return accumulator + amount
+  }, 0);
+
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${donos}</span> of <span className="secondary">${targetAmount}</span>
+      </h2>
+    </section>
+  )
 };
 
 export default Progress;

60503e20fab9077a86b15ba4b5be52ea9f3d988e
Fadila-Ali <fadilaali@pursuit.org>
2022-10-06 18:17:14 -0400

    tests passed

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,20 @@
-const RecentDonations = () => {
-  return null;
+const RecentDonations = ({ donations }) => {
+  return (
+    <div>
+      <ul>
+        {Object.values(donations).map(({ name, amount, caption }) => {
+          return (
+            <li>
+              <span>
+                {name} donated ${amount}
+              </span>
+              {caption}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default RecentDonations;

74213a2552e0cc07b27fe7beb36339a00ad57085
NRiddick113 <NatashaRiddick@pursuit.org>
2022-10-06 19:02:18 -0400

    Donation Form

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,5 +1,25 @@
-const DonationForm = () => {
-  return null;
+const DonationForm = ({donations}) => {
+  return (
+    <div>
+       <h3>You could be donation <span className="secondary">#{donations.length + 1}!</span></h3>
+   <form>
+     <label htmlFor="name">Name
+    <br></br>
+    <input id="name" name="name" type="text" placeholder="Your name..." /></label>
+    <br></br>
+    <label htmlFor="caption">Caption
+    <br></br>
+    <input id="caption" name="caption" type="text" placeholder="Add a brief message..." /></label>
+    <br></br>
+    <label htmlFor="amount">Amount
+    <br></br>
+    <input id="amount" name="amount" type="number" placeholder="0"/></label>
+    <p></p>
+    <button>Donate!</button>
+  </form>
+    </div>
+  )
+
 };
 
 export default DonationForm;
--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,18 @@
-const Progress = () => {
-  return null;
+const Progress = ({donations, targetAmount}) => {
+  const sum = (donations) => {
+    let sum = 0;
+    for (let donate of donations) {
+     sum += parseInt(donate.amount);
+    }
+    return sum
+  };
+  const total = sum(donations);
+  return (
+    <h2>
+    Raised <span className="secondary">${total}</span> of
+    <span className="secondary"> ${targetAmount}</span>
+  </h2>
+  )
 };
 
 export default Progress;

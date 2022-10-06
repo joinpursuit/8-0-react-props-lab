1be2cea30756ab62005a4e074e16fc4ce6c46d3b
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2022-10-06 17:56:59 -0400

    passing all

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,17 @@
-const Progress = () => {
-  return null;
+const Progress = ({ targetAmount, donations }) => {
+  const totalAmount = donations.reduce((acc, { amount }) => {
+    return acc + amount;
+  }, 0);
+
+  // const ProgressSoFar = totalAmount(donations);
+
+  return (
+    <section>
+      <h2>
+        Raised <span> ${totalAmount}</span> of <span>${targetAmount}</span>
+      </h2>
+    </section>
+  );
 };
 
 export default Progress;

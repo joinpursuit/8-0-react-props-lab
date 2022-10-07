dad272c8119837e01f5baabd5f8d59762113f270
Richie-Pursuit <richiecarrasco@pursuit.org>
2022-10-07 05:09:58 -0400

    learning curve

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import TopBar from "./Components/TopBar";
+import RecentDonations from "./Components/RecentDonations";
+import Progress from "./Components/Progress";
+import DonationForm from "./Components/DonationForm";
 import "./App.css";
 
 const targetAmount = 1000;
@@ -42,10 +45,12 @@ export default class App extends React.Component {
       <>
         <TopBar />
         <main className="container">
-          <section className="sidebar">{/* Recent Donations */}</section>
+        <section className="sidebar">
+            <RecentDonations donations={donations} />
+          </section>
           <section className="">
-            {/* Progress */}
-            {/* Donation Form */}
+          <Progress donations={donations} targetAmount={targetAmount} />
+            <DonationForm donations={donations} />
           </section>
         </main>
       </>
--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,16 @@
-const Progress = () => {
-  return null;
+const Progress = ({targetAmount, donations}) => {
+  
+  const totalDon = donations.reduce((accumilator, { amount }) => {
+    return accumilator + amount}, 0)
+  
+  return (
+    <section>
+      <h2>
+        Raised <span> ${totalDon}</span> of 
+        <span>${targetAmount}</span>
+      </h2>
+    </section>
+  )
 };
 
 export default Progress;

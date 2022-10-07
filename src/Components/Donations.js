1b534eb5881845d9b84156b3fd23fa15cadd31c5
D-Stukes <dstukes888@gmail.com>
2022-10-07 06:22:53 -0400

    added 2 empty ccomponents - TargetAmount and Donations

---
+++ src/Components/Donations.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+
+ import Component from './Component'; 
+
+function Donations() {
+    return (
+        <div className='donations'>
+            <h3></h3>
+            <ul>
+       
+            </ul>
+        </div>
+    );
+}
+
+export default Donations;
---
+++ src/Components/TargetAmount.js
@@ -0,0 +1,14 @@
+<!-- import Component from './Component'; -->
+
+function TargetAmount() {
+    return (
+        <div className='target'>
+            <h3></h3>
+            <ul>
+       
+            </ul>
+        </div>
+    );
+}
+
+export default TargetAmount;

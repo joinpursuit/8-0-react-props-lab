2b274fe414d5ce29a0a72b4de9eafab3d7ba1722
shaquala <shaqualafredericks@pursuit.org>
2022-10-06 23:58:26 -0400

    update app

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,5 +1,35 @@
-const DonationForm = () => {
-  return null;
+const DonationForm = (props) => {
+  const { donationcount } = props
+  return (
+    <section className="donation">
+      <h3>You could be donation <span className="secondary">#{donationcount}!</span></h3>
+      <form>
+        <label
+        >Name<input
+        id="name"
+        type="text"
+        placeholder="Your name ..."/>
+    
+        </label>
+        <label>
+          Caption<input
+          id="caption"
+          name="caption"
+          type="text"
+          placeholder="Add a brief message..."/>
+        </label>
+        <label>
+          Amount<input
+          id="amount"
+          name="amount"
+          type="number"
+          placeholder="0"/>
+        
+        </label>
+        <button>Donate!</button>
+      </form>
+    </section>
+  )
 };
 
 export default DonationForm;

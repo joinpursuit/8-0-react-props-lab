2771c3de3f70f70483f9587931ac2a1ccdbeba54
MarsiaRochelin <marsrochelin@pursuit.org>
2022-10-07 10:04:30 -0400

    completed lab

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,15 @@
-const RecentDonations = () => {
-  return null;
+const RecentDonations = ({ donations }) => {
+  return (
+    <ul> {donations.map(donation => {
+     return <li>
+              <span>
+                {donation.name} donated ${donation.amount}
+              </span>
+              {donation.caption}
+            </li>
+            })}
+    </ul>
+  );
 };
 
 export default RecentDonations;

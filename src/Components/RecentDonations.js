27bc8b40b80ab749173b1a7bede821636cbd7f11
Bo Latt <bolattny@gmail.com>
2022-10-09 11:32:58 -0400

    passed the tests

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,19 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations({ donations }) {
+  return (
+    <>
+      <h2>Recent Donations</h2>
+      <ul>
+        {donations.map((d) => {
+          return (
+            <li key={d.id}>
+              <span>
+                {d.name} donated ${d.amount}
+              </span>
+              {d.caption}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }

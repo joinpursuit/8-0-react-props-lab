5e582c5759cfb0b1614c5dc1f66d49e927f59427
i-guerrero <isaacguerrero@pursuit.org>
2022-10-09 12:38:59 -0400

    Passing all tests

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,14 @@
-export default function Progress() {
-  return null;
+export default function Progress({ donations, targetAmount }) {
+  return (
+    <section className="progress">
+      <h2>
+        Raised{" "}
+        <span className="secondary">
+          ${donations.reduce((acc, curr) => acc + curr.amount, 0)}
+        </span>{" "}
+        of
+        <span className="secondary"> ${targetAmount}</span>
+      </h2>
+    </section>
+  );
 }

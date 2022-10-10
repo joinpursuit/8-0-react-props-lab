9be7d1efeb7bf128ea0c8efcc5470f16717b84c0
Karyn Huston <huston.karyn@gmail.com>
2022-10-10 12:26:02 -0400

    missing a space line 8 progress.js costed me hours! so fun

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,12 @@
-export default function Progress() {
-  return null;
+export default function Progress(props) {
+  const { donoTotal, targetAmount } = props;
+
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${donoTotal}</span> of
+        <span className="secondary"> ${targetAmount}</span>
+      </h2>
+    </section>
+  );
 }

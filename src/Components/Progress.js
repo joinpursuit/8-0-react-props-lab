63cb05c0a7192bdeceade32da053d214ce933b9b
cmm-7 <carlosmendoza@pursuit.org>
2022-10-09 22:31:36 -0400

    react-props-lab complete

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,15 @@
-export default function Progress() {
-  return null;
+export default function Progress(prop) {
+  const { donations } = prop;
+  let progress = 0 ;
+  donations.map((donation) => {
+    console.log(donation.amount);
+    return progress += donation.amount});
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${progress}</span> of
+        <span className="secondary"> $1000</span>
+      </h2>
+    </section>
+  );
 }

f4c93c74b9e29b760d96be6b15256726d28eb8ea
Tech-Kat <katpelle@pursuit.org>
2022-10-10 15:34:14 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import TopBar from "./Components/TopBar";
+import DonationForm from "./Components/DonationForm";
+import RecentDonations from "./Components/RecentDonations";
+import Progress from "./Components/Progress";
 import "./App.css";
 
 const targetAmount = 1000;
@@ -35,16 +38,24 @@ const donations = [
     name: "Sam",
   },
 ];
+const getTotal = (donations) => {
+  let sum = 0;
+  for (let donated of donations) {
+    sum += donated.amount
+  }
+  return sum.toFixed(2)
+};
+const total = getTotal(donations)
 
 function App() {
   return (
     <>
       <TopBar />
       <main className="container">
-        <section className="sidebar">{/* Recent Donations */}</section>
+        <section className="sidebar"><RecentDonations donations={donations}/></section>
         <section className="">
-          {/* Progress */}
-          {/* Donation Form */}
+          <Progress targetAmount = {targetAmount} total={total}/>
+          <DonationForm donations={donations} />
         </section>
       </main>
     </>
--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,13 @@
-export default function Progress() {
-  return null;
-}
+const Progress = ({targetAmount, total}) => {
+  
+
+  return (
+    <section className="progress">
+  <h2>
+    Raised <span className="second">${total}</span> of
+    <span className="second"> ${targetAmount}</span>
+  </h2>
+</section>
+  )
+};
+export default Progress;

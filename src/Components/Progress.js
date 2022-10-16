94422b2261cf393ec488413c186ef7748c152324
rodicach <rodicachavez@pursuit.org>
2022-10-16 11:52:13 -0400

    Passing test

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,11 @@
-export default function Progress() {
-  return null;
-}
+const Progress = ({ targetAmount, total }) => {
+	return (
+		<section className="progress">
+			<h2>
+				Raised <span className="secondary">${total}</span> of
+				<span className="secondary"> ${targetAmount}</span>
+			</h2>
+		</section>
+	);
+};
+export default Progress;
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,16 @@
-export default function RecentDonations() {
-  return null;
+const RecentDonations = ({ donations }) => {
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+        <ul>
+          {donations.map(({name, amount, caption}) => {
+            return (
+              <li key={name}>{name} donated ${amount} - {caption}</li>
+            );
+          })}
+        </ul>
+    </section>
+  )
 }
+
+export default RecentDonations;

8c72f47dcb3d43abb206cdca8904ba7d5d1f397e
dilrubamajumder <dilrubamajumder@pursuit.org>
2022-10-16 21:24:38 -0400

    completed and passed everything

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,21 @@
-export default function RecentDonations() {
-  return null;
+// export default function RecentDonations() {
+//   return null;
+// }
+
+import React from "react";
+
+const RecentDonations= ({donations}) =>{
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        {donations.map((donation)=> (  <li key={donation.id}> 
+            <span>{donation.name} donated ${donation.amount}</span>{donation.caption}
+          </li>))}
+         
+      </ul>
+    </section>
+  );
 }
+
+export default RecentDonations;

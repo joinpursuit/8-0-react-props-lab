ee93d4658cb4d764e358802f610032320f6d089b
cjs2408 <carlossuero@pursuit.org>
2022-10-22 12:51:14 -0400

    Done part 2

--- src/helper.js
+++
@@ -1,9 +0,0 @@
-
-export const getTotal = (donations) => {
-    const total =
-      donations.reduce((acc, currVal) => {
-        return acc + parseInt(currVal.amount);
-      }, 0);
-  
-    return total;
-  };

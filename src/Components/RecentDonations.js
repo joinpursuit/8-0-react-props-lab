b759f5c44e4c7464b208ba9829e6f170a317f772
jamesscott@pursuit.org <Jimmy@2019mbp.local>
2022-10-29 19:56:46 -0400

    Initial Commit

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,16 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations({ donations }) {
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        {
+          donations.map(donation => {
+            return (
+              <li><span>{donation.name} dontated ${donation.amount}</span>{donation.caption}</li>
+            )
+          })
+        }
+      </ul>
+    </section>
+  );
 }

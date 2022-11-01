8e27ee049a8f1c0ae0182459e7fdf53361b3473f
melanieinpursuit <melaniegonzalez@pursuit.org>
2022-11-01 11:33:08 -0400

    Finished lab.

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,3 +1,39 @@
-export default function DonationForm() {
-  return null;
+const DonationForm = ({ donations }) => {
+  return (
+    <>
+      <section className="donation">
+        <h3>
+          You could be donation #
+          <span className="secondary">{donations.length + 1}!</span>
+        </h3>
+        <form>
+          <label htmlFor="name">
+            Name
+            <input
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Your name..."
+            />
+          </label>
+          <label htmlFor="caption">
+            Caption
+            <input
+              id="caption"
+              name="caption"
+              type="text"
+              placeholder="Add a brief message..."
+            />
+          </label>
+          <label htmlFor="amount">
+            Amount
+            <input id="amount" name="amount" type="number" placeholder="0" />
+          </label>
+          <button>Donate!</button>
+        </form>
+      </section>
+    </>
+  );
 }
+
+export default DonationForm;
